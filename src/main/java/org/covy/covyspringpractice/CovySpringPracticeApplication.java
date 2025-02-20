a94bbdf31f0e32f715775ceef9c9cd451e6e4986
이민구 <goorm@goormui-MacBookPro.local>
2025-02-20 23:43:22 +0900

    Init: Initial commit

---
+++ src/main/java/org/covy/covyspringpractice/CovySpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.covy.covyspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovySpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovySpringPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/covy/covyspringpractice/CovySpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.covy.covyspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovySpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
