ebc81304c7617b62a230f1f40c0e70da86361fd3
이민구 <goorm@goormui-MacBookPro.local>
2025-02-22 15:26:00 +0900

    Fix: 멤버 서비스 리포를 사용하도록 수정

--- src/main/java/org/covy/covyspringpractice/service/MemberService.java
+++ src/main/java/org/covy/covyspringpractice/service/MemberService.java
@@ -1,25 +1,27 @@
 package org.covy.covyspringpractice.service;
 
+import org.covy.covyspringpractice.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class MemberService {
-    // RDB 대신 해시맵에 데이터를 저장하는 방식으로 실습 진행
-    private final Map<String, String> memberStorage = new HashMap<>();
 
-    // 회원 생성
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void createMember(String name) {
-        // Map에 key=value로 저장
-        memberStorage.put(name, "회원 정보: " + name);
-        System.out.println("MemberService: 회원 '" + name + "'이(가) 생성되었습니다.");
+        memberRepository.createMember(name);
+        System.out.println("MemberService: 회원 '" + name + "' 생성 로직 실행 완료.");
     }
 
-    // 회원 정보 조회
     public String getMemberInfo(String name) {
-        // 저장된 값이 없다면 null 또는 "존재하지 않는 회원" 등의 메시지를 반환하도록 처리
-        return memberStorage.getOrDefault(name, "존재하지 않는 회원입니다.");
+        return memberRepository.findMember(name);
     }
+
 }
